05f47d7f6bba70a816b896be7d41b31dfe6b413a
anastasiaduplina <anastasiaduplina@yandex.ru>
2024-11-15 13:36:21 +0300

    fix

--- get_vacancies.py
+++ get_vacancies.py
@@ -56,9 +56,9 @@ from jobs_db import *
 
 
 def get_vacancies():
-    ids = [156]
-        # 156, 160, 10, 12, 150, 25, 165, 34, 36, 73, 155,
-        # 96, 164, 104, 157, 107, 112, 113, 148, 114, 116, 121, 124, 125, 126]
+    ids = [
+        156, 160, 10, 12, 150, 25, 165, 34, 36, 73, 155,
+        96, 164, 104, 157, 107, 112, 113, 148, 114, 116, 121, 124, 125, 126]
 
     vacanciess = []
     for i in ids:
@@ -85,7 +85,7 @@ def get_vacancies_by_role(role: int):
     pages = res["pages"]
     print(role, pages)
     vacanciess = []
-    for i in range(1):
+    for i in range(pages):
         params = {
             "professional_role": role,
             "per_page": 100,
@@ -124,9 +124,6 @@ def get_vacancies_by_role(role: int):
     return vacanciess
 
 
-# a=get_vacancies()
-# print(len(a))
-list=db_methods.get_all_vacancies()
-print(len(list))
-print(list[0])
+get_vacancies()
+
 
