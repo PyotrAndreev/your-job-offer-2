426c68cc1b59808d43b8b4036a94694ece16055a
Zaitseva Daria <zaitseva.dr@phystech.edu>
2024-12-04 09:01:41 +0300

    changed table name

--- your_job_offer/models/hh_token.py
+++ your_job_offer/models/hh_token.py
@@ -15,7 +15,7 @@ class HH_Token(Base):
         access_token (str): The access token used for authentication with the hh.ru API.
         refresh_token (str): The refresh token used to obtain a new access token when the old one expires.
     """
-    __tablename__ = "hh_tokens"
+    __tablename__ = "tokens"
     id = Column(Integer, primary_key=True)
     login = Column(String(50), nullable=False, name="login")
     access_token = Column(String(512), nullable=False, name="access")
