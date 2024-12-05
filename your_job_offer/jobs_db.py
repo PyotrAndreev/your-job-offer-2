2282bc661e9eade387c248cac56534dd887a4c85
anastasiaduplina <anastasiaduplina@yandex.ru>
2024-12-05 08:35:12 +0300

    some fix db

--- your_job_offer/jobs_db.py
+++ your_job_offer/jobs_db.py
@@ -3,6 +3,9 @@ from sqlalchemy import (
 )
 from repository.vacancies_repository.db_session import Base
 
+from models.vacancy import Vacancy
+from models.user import Country, City, Language, Skill, Job, Education, WorkExperience, User, Project, Achievement
+
 engine = create_engine(
     "postgresql+psycopg2://postgres:password@db:5432/jobs"
 )
--- your_job_offer/tokens_db.py
+++ your_job_offer/tokens_db.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from repository.tokens_repository.db_session import Base
+from models.hh_token import HH_Token
 
 engine = create_engine("postgresql+psycopg2://postgres:password@db-tokens:5432/tokens")
 
