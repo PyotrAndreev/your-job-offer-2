02124b6ce98fc937d6353b8407e85f85b0b41928
Zaitseva Daria <zaitseva.dr@phystech.edu>
2024-12-12 07:04:59 +0300

    fix

--- your_job_offer/services/api_server/api.py
+++ your_job_offer/services/api_server/api.py
@@ -228,28 +228,46 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/upload', methods=['POST'])
 def upload_file():
     try:
-        data = request.json
-        if 'file' not in request.files or not data or "login" not in data or "password" not in data:
-            return jsonify({'message': 'No file part in the request or no data in request'}), 400
+        # data = request.json
+        # if 'file' not in request.files or not data or "login" not in data or "password" not in data:
+        #     return jsonify({'message': 'No file part in the request or no data in request'}), 400
+        #
+        # file = request.files['file']
+        # login = data.get('login')
+        # password = data.get('password')
+        #
+        # if file.filename == '':
+        #     return jsonify({'message': 'No file selected'}), 400
+        #
+        # if file:
+        #     file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        #     file.save(file_path)
+        #     user = parse(file_path)
+        #     log.info(user.__str__)
+        #     user.login = login
+        #     user.password = password
+        #     update_user(user)
+        #     return jsonify(user.to_json()), 200
+        # log.error("file upload failed")
+        # return jsonify({'message': 'File upload failed'}), 500
 
-        file = request.files['file']
-        login = data.get('login')
-        password = data.get('password')
+        file_bytes = request.data
 
-        if file.filename == '':
-            return jsonify({'message': 'No file selected'}), 400
+        if not file_bytes:
+            return jsonify({"error": "Empty request body"}), 400
 
-        if file:
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(file_path)
-            user = parse(file_path)
-            log.info(user.__str__)
-            user.login = login
-            user.password = password
-            update_user(user)
-            return jsonify(user.to_json()), 200
-        log.error("file upload failed")
-        return jsonify({'message': 'File upload failed'}), 500
+        file_name = request.headers.get('X-File-Name', 'uploaded_file.pdf')
+        file_path = os.path.join(UPLOAD_FOLDER, file_name)
+
+        with open(file_path, 'wb') as file:
+            file.write(file_bytes)
+        user = parse(file_path)
+        log.info(user.__str__)
+        user.login = "test"
+        user.password = "test"
+        update_user(user)
+        return jsonify(user.to_json()), 200
+
     except Exception as e:
         log.error(f"Ошибка загрузки резюме: {e}")
         return make_response(jsonify({"error": str(e)}), 500)
