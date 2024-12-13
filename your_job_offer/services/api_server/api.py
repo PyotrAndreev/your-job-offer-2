a19a05d9e9fafeb152f23b0d835bb3e07ae40b52
Zaitseva Daria <zaitseva.dr@phystech.edu>
2024-12-13 18:50:02 +0300

    fix

--- your_job_offer/services/api_server/api.py
+++ your_job_offer/services/api_server/api.py
@@ -27,13 +27,14 @@ from your_job_offer.services.cv_parser.methods import parse
 from your_job_offer.repository.vacancies_repository.get_professional_roles import get_professional_roles
 
 from datetime import date, datetime
+from your_job_offer.entities.enums import GenderEnum
 
 def json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
 
     if isinstance(obj, (datetime, date)):
         return obj.isoformat()
-    if isinstance(obj, Enum):
+    if isinstance(obj, type(GenderEnum)):
         return obj.value
 
     raise TypeError ("Type %s not serializable" % type(obj))
