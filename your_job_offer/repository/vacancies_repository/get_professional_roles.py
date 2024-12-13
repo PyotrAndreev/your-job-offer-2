c7b7b1e81dcfa3ff26db0e46325f6faaf5982eb6
Zaitseva Daria <zaitseva.dr@phystech.edu>
2024-12-13 17:37:28 +0300

    added professional roles

--- your_job_offer/repository/vacancies_repository/get_professional_roles.py
+++ your_job_offer/repository/vacancies_repository/get_professional_roles.py
@@ -2,10 +2,10 @@ from your_job_offer.repository.vacancies_repository.utils import professional_ro
 from your_job_offer.repository.vacancies_repository.db_methods import save_role
 import your_job_offer.logger as logger
 
-log - logger.get_logger(__name__)
+log = logger.get_logger(__name__)
 
 def get_professional_roles():
     for role in professional_roles:
         professional_role = ProfessionalRole(roleId=role["id"], name=role["role"])
-        logger.info(f"Saved {professional_role}")
+        log.info(f"Saved {professional_role}")
         save_role(professional_role)
