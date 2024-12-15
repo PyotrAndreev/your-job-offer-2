c879535c515b7979a63ca40a05b5179f0c02e49e
Zaitseva Daria <zaitseva.dr@phystech.edu>
2024-12-16 01:05:20 +0300

    test

--- your_job_offer/repository/vacancies_repository/db_methods.py
+++ your_job_offer/repository/vacancies_repository/db_methods.py
@@ -364,6 +364,7 @@ def get_vacancies_by_user(user: User):
 def update_user(updated_user: UserModel):
     try:
         user = get_user(updated_user.login)
+        log.info(f"update user: {updated_user.login}")
         if updated_user.city:
             if not if_exist_city(updated_user.city.name):
                 save_city(
@@ -392,6 +393,7 @@ def update_user(updated_user: UserModel):
 
         if updated_user.professional_role:
             user.roleId = updated_user.professional_role.role_id
+
         user.birthDate = updated_user.birth_date
         user.firstName = updated_user.first_name
         user.lastName = updated_user.last_name
