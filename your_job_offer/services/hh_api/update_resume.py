f037e2293addd59bdb02471f599138445a514d43
Zaitseva Daria <zaitseva.dr@phystech.edu>
2024-12-16 18:20:21 +0300

    fix

--- your_job_offer/services/hh_api/update_resume.py
+++ your_job_offer/services/hh_api/update_resume.py
@@ -23,13 +23,13 @@ def update_resume(user: UserModel, access_token: str):
         "first_name": user.first_name,
         "last_name": user.last_name,
         "middle_name": user.middle_name if user.middle_name != '' else None,
-        "birth_date": user.birth_date,
+        "birth_date": user.birth_date[:10] if user.birth_date != '' and user.birth_date else None,
         "gender": {
             "id": user.gender.value if user.gender else None,
         },
         "area": {
             "id": user.city.area_id,
-        } if user.city else None,
+        } if user.city else 1,
         "citizenship": [
             {
                 "id": user.country.area_id if user.country and user.country.area_id else 113,
@@ -50,27 +50,27 @@ def update_resume(user: UserModel, access_token: str):
                     "name": "Мобильный телефон",
                 },
                 "value": {
-                    "formatted": 79806253660,
+                    "formatted": user.phone,
                 },
             },
         ],
         "skill_set": [f"{skill.name}: {skill.description}" for skill in user.skills]
         if user.skills
         else None,
-        # "experience": [
-        #     {
-        #         "company": exp.work_place,
-        #         "position": exp.job,
-        #         "description": exp.description,
-        #         "start": (
-        #             exp.start_date.isoformat() if exp.start_date else None
-        #         ),
-        #         "end": (
-        #             exp.finish_date.isoformat() if exp.finish_date else None
-        #         ),
-        #     }
-        #     for exp in user.work_experiences
-        # ],
+        "experience": [
+            {
+                "company": exp.work_place,
+                "position": exp.job,
+                "description": exp.description,
+                "start": (
+                    exp.start_date[:10] if exp.start_date else None
+                ),
+                "end": (
+                    exp.finish_date[:10] if exp.finish_date else None
+                ),
+            }
+            for exp in user.work_experiences
+        ],
         "education": {
             "level": {
                 "id": user.education_level.value,
@@ -101,36 +101,35 @@ def update_resume(user: UserModel, access_token: str):
                 "currency": "RUR",
             }
             if user.min_salary
-            else None
-        ,
-        # "relocation":
-        #     {
-        #         "type":
-        #             {
-        #                 "id": user.relocation.value,
-        #                 "name": relocation_id_name.get(user.relocation.value),
-        #             },
-        #     }
-        #     if user.relocation else None,
-        # "business_trip_readiness": {
-        #     "id": user.business_trip_readiness.value
-        # }
-        # if user.business_trip_readiness else None,
-        # "employments": [
-        #     {
-        #         "id": user.employment.value,
-        #         "name": employment_id_name.get(user.employment.value),
-        #     },
-        # ]
-        # if user.employment else None,
-        # "schedules":
-        #     [
-        #         {
-        #             "id": user.schedule.value,
-        #             "name": schedule_id_name.get(user.schedule.value)
-        #         },
-        #     ]
-        #     if user.schedule else None,
+            else None,
+        "relocation":
+            {
+                "type":
+                    {
+                        "id": user.relocation.value,
+                        "name": relocation_id_name.get(user.relocation.value),
+                    },
+            }
+            if user.relocation else None,
+        "business_trip_readiness": {
+            "id": user.business_trip_readiness.value
+        }
+        if user.business_trip_readiness else None,
+        "employments": [
+            {
+                "id": user.employment.value,
+                "name": employment_id_name.get(user.employment.value),
+            },
+        ]
+        if user.employment else None,
+        "schedules":
+            [
+                {
+                    "id": user.schedule.value,
+                    "name": schedule_id_name.get(user.schedule.value)
+                },
+            ]
+            if user.schedule else None,
         "professional_roles": [
             {
                 "id": user.professional_role.role_id,
