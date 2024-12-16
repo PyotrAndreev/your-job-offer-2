517ab3498214793d0bedbbb653ff7eb93be3bee3
afarovruslan <afarovruslan@gmail.com>
2024-12-16 15:37:58 +0300

    теперь Subject необязателен в письмах

--- your_job_offer/use_cases/tracking/methods.py
+++ your_job_offer/use_cases/tracking/methods.py
@@ -1,7 +1,6 @@
 from your_job_offer.use_cases.user_cases import get_all_users
 from your_job_offer.use_cases.tracking.internal import parse_for_user, log
 
-
 def parse():
     users = get_all_users()
     log.info(f"начинаю обновление статусов, юзеров {len(users)}")
