9a54f4e2b577ae9825886fa887a63ba574f447b3
anastasiaduplina <anastasiaduplina@yandex.ru>
2024-12-16 11:10:41 +0300

    fix email

--- your_job_offer/use_cases/user_cases.py
+++ your_job_offer/use_cases/user_cases.py
@@ -73,18 +73,3 @@ def get_vacancies_by_keys(
             vacancies.append(None)
     return vacancies
 
-
-def extract_emails_and_passwords(file_path: str):
-    emails = []
-    passwords = []
-
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-            line = line.strip()
-            if ':' in line:
-                email, password = line.split(':', 1)
-                emails.append(email)
-                passwords.append(password)
-
-    return emails, passwords
